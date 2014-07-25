82785104fa36634cd16f300f434bc9cba1cd5b93
Alexander Groszewski <groszewa@gmail.com>
2014-07-24 23:59:09 -0500

    Got inputs through to PHP

---
+++ php/SimpleStore.php
@@ -0,0 +1,30 @@
+<?php
+//Enter your IDs
+define("Access_Key_ID", "AKIAJGQHTY3M674DX2NQ");
+define("Associate_tag", "gooenogif-20");
+//Set up the operation in the request
+function ItemSearch($SearchIndex, $Keywords){
+//Set the values for some of the parameters
+$Operation = "ItemSearch";
+$Version = "2013-08-01";
+$ResponseGroup = "ItemAttributes,Offers";
+//User interface provides values
+//for $SearchIndex and $Keywords
+//Define the request
+$request=
+ "http://webservices.amazon.com/onca/xml"
+ . "?Service=AWSECommerceService"
+ . "&AssociateTag=" . Associate_tag
+ . "&AWSAccessKeyId=" . Access_Key_ID
+ . "&Operation=" . $Operation
+ . "&Version=" . $Version
+ . "&SearchIndex=" . $SearchIndex
+ . "&Keywords=" . $Keywords
+ . "&Signature=" . "Request Signature"
+ . "&ResponseGroup=" . $ResponseGroup;
+//Catch the response in the $response object
+$response = file_get_contents($request);
+$parsed_xml = simplexml_load_string($response);
+printSearchResults($parsed_xml, $SearchIndex);
+}
+?>
