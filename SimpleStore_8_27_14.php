487ae52e0af3ed64b035596317941efd62782470
matterrr <matterrr>
2014-07-27 16:46:17 -0700

    Methods for accessing AWS info
    Updated amazon_scripts with paths to AWS info

---
+++ SimpleStore_8_27_14.php
@@ -0,0 +1,63 @@
+<?php
+include('amazon_scripts.php');
+    
+define('AWS_ACCESS_KEY_ID', get_aws_access_key_id());
+define('AWS_SECRET_ACCESS_KEY', get_aws_secret_access_key());
+define('AMAZON_ASSOC_TAG', 'gooenogif-20');
+
+function amazon_get_signed_url($searchTerm) {
+	$base_url = "http://ecs.ama[]zonaws.com/onca/xml";
+	$params = array(
+		'AWSAccessKeyId' => AWS_ACCESS_KEY_ID,
+		'AssociateTag' => AMAZON_ASSOC_TAG,
+		'Version' => "2010-11-01",
+		'Operation' => "ItemSearch",
+		'Service' => "AWSECommerceService",
+		'Availability' => "Available",
+		'Condition' => "New",
+		'Operation' => "ItemSearch",
+		'MinimumPrice' => "3500",
+		'MaximumPrice' => "7500",
+		'MerchantId' => "Amazon",
+		'ItemPage' => "1",
+		'SearchIndex' => 'Music', //Change search index if required, you can also accept it as a parameter for the current method like $searchTerm
+		'Keywords' => $searchTerm);
+
+
+		//'ResponseGroup'=>"Images,ItemAttributes,EditorialReview",
+	
+	if(empty($params['AssociateTag'])) {
+		unset($params['AssociateTag']);
+	}
+		
+	// Add the Timestamp
+	$params['Timestamp'] = gmdate("Y-m-d\TH:i:s.\\0\\0\\0\\Z", time());
+	 
+	// Sort the URL parameters
+	$url_parts = array();
+	foreach(array_keys($params) as $key)
+		$url_parts[] = $key . "=" . str_replace('%7E', '~', rawurlencode($params[$key]));
+	sort($url_parts);
+	 
+	// Construct the string to sign
+	$url_string = implode("&", $url_parts);
+	$string_to_sign = "GET\necs.amazonaws.com\n/onca/xml\n" . $url_string;
+
+	// Sign the request
+	$signature = hash_hmac("sha256", $string_to_sign, AWS_SECRET_ACCESS_KEY, TRUE);
+	
+	// Base64 encode the signature and make it URL safe
+	$signature = urlencode(base64_encode($signature));
+	 
+	$url = $base_url . '?' . $url_string . "&Signature=" . $signature;
+	
+	return ($url);
+}
+
+$url = amazon_get_signed_url("radiohead");
+
+//Below is just sample request dispatch and response parsing for example purposes.
+echo $url;
+
+
+?>
