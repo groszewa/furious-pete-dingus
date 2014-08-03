6d3266cce555e1716dd3b710cfd2a6fe20a41f4d
matterrr <matterrr>
2014-08-03 11:23:42 -0700

    Generate results for all rows in table
    update_results_db.php now looks at every row in the search table and
    generates a search URL. It creates a php array of these URLs and dumps
    the parsed results in the results table.
    
    Next steps:
    - Using multiple search tables into multiple results tables
    - Sanitizing SQL results
    - Determining number of page results and return results for that many
    pages
    - Remove low quality results
    - Log searches that don't produce many results

--- php/update_results_db.php
+++ php/update_results_db.php
@@ -1,11 +1,16 @@
 <?php
 
+//////////////////////////////////////
+//                                  //
+//        ITEM SEARCH SECTION       //
+//                                  //
+//////////////////////////////////////
+
 include('amazon_scripts.php'); 
 
 $user = 'root';
 $pass = '';
 $search_db = 'item_search_db';
-$results_db = 'search_results_db';
 
 // Set timezone
 date_default_timezone_set('America/Los_Angeles');
@@ -14,35 +19,10 @@ date_default_timezone_set('America/Los_Angeles');
 $con_search = new mysqli('localhost',$user,$pass,$search_db);
 
 // Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if ($con_search->connect_errno) {
+    printf("Connect failed: %s\n", $con_search->connect_error);
+    exit();
 }
-
-// Get the column names from our SQL table
-$sql_column_names = mysqli_query($con_search,"SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` 
-WHERE `TABLE_SCHEMA`='item_search_db' AND `TABLE_NAME`='dad_search_table'");
-
-// Put column names in PHP array
-$column_names = [];
-while($row = mysqli_fetch_array($sql_column_names))
-{
-    $column_names[] = $row[0];
-}
-
-// Get the values from our table for the item search operation
-$sql_search_values = mysqli_query($con_search,"SELECT * FROM dad_search_table WHERE `Id`=1");
-$search_values = mysqli_fetch_array($sql_search_values,MYSQLI_NUM);
-
-// Close connection to DB
-$con_search->close();
-
-// Combine the two arrays (elementName => elementValue)
-$item_search = array_combine($column_names, $search_values);
-
-//print_r($item_search);
-
-// Filter out all empty element values
-$item_search_filtered = array_filter($item_search);
 
 // Generate the necessary constant element names and values
 $constant_elements = array(
@@ -52,81 +32,112 @@ $constant_elements = array(
     'Operation' => "ItemSearch",
     'Service' => "AWSECommerceService");
 
-// Add the constant elements to our array
-$params = $constant_elements + $item_search_filtered;
+// Get the column names from our SQL table
+$sql_column_names = $con_search->query("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+WHERE `TABLE_SCHEMA`='item_search_db' AND `TABLE_NAME`='dad_search_table'");
 
-// Generate item search URL
-$request = amazon_get_signed_url($params);
-
-echo $request . "<br>";
-
-// Catch response in xml object
-$response = file_get_contents($request);
-
-// Parse xml
-$parsed_xml = simplexml_load_string($response);
-
-// Connect to MySQL results table
-$con_results = new mysqli('localhost',$user,$pass,$results_db);
-
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (!$sql_column_names) { // add this check.
+    die('Invalid query on line 39: ' . mysql_error());
 }
 
-// Parse xml into PHP array
-$numOfItems = $parsed_xml->Items->TotalResults;
-$item_results_array = array(
-    'Id' => 0,
-    'Actor' => "",
-    'Artist' => "",
-    'Author' => "",
-    'CorrectedQuery' => "",
-    'Director' => "",
-    'Keywords' => "",
-    'Manufacturer' => "",
-    'ProductGroup' => "",
-    'Title' => "",
-    'LastUpdated' => "");
-
-// print_r($results_array);
-
-if($numOfItems > 0){
-    foreach($parsed_xml->Items->Item as $current){
-        foreach($item_results_array as $entry=>$value){
-            if (isset($current->ItemAttributes->$entry)) {
-                // print $current->ItemAttributes->$entry . "<br>";
-                // print "Entry = " . $entry . "<br>";
-                $item_results_array[$entry] = (string)$current->ItemAttributes->$entry;   
-            }            
-        }
-    }
-    
-    if (isset($parsed_xml->Items->Item->ASIN)) {
-        $item_results_array['ASIN'] = (string)$parsed_xml->Items->Item->ASIN;
-    }
-    
-    if (isset($parsed_xml->Items->Item->DetailPageURL)) {
-        $item_results_array['DetailPageURL'] = (string)$parsed_xml->Items->Item->DetailPageURL;
-    }
-    
-    $item_results_array['LastUpdated'] = date("Y-m-d H:i:s");   
+// Put column names in PHP array
+$column_names = [];
+while($row = $sql_column_names->fetch_array())
+{
+    $column_names[] = $row[0];
+    echo $row[0] . "<br>";
 }
 
-$sql = sprintf(
-    'INSERT INTO dad_results_table (%s) VALUES ("%s")',
-    implode(',',array_keys($item_results_array)),
-    implode('","',array_values($item_results_array))
-);
+$sql_column_names->close();
 
-echo "<br>" . $sql;
+echo "Hello world <br>";
 
-if (!$con_results->query($sql)) {
-    printf("Errormessage: %s\n", $con_results->error);
+// Get the values from our table for the item search operation
+$sql_search_values = $con_search->query("SELECT * FROM dad_search_table");
+if (!$sql_search_values) { // add this check.
+    die('Invalid query on line 54: ' . mysql_error());
 }
 
+$requests = array();
 
-$con_results->close();
-// Dump PHP array into MySQL row
+// Iterate over each row
+while($search_values = $sql_search_values->fetch_array(MYSQL_NUM)){
+    $item_search = array_combine($column_names, $search_values);    // Combine the two arrays (elementName => elementValue)
+    $item_search_filtered = array_filter($item_search);             // Filter out all empty element values
+    $params = $constant_elements + $item_search_filtered;           // Add the constant elements to our array
+    unset($params['Id']);
+    unset($params['DateUpdated']);
+    
+    $requests[] = amazon_get_signed_url($params);                   // Generate item search URL
+}
+
+print_r($requests);
+
+//////////////////////////////////////
+//                                  //
+//      RESULTS PARSING SECTION     //
+//                                  //
+//////////////////////////////////////
+
+// Delete contents of MySQL table
+$con_search->query("TRUNCATE TABLE dad_results_table") or die("Could not delete table");
+
+foreach ($requests as $request) {
+    // Catch response in xml object
+    $response = file_get_contents($request);
+
+    // Parse xml
+    $parsed_xml = simplexml_load_string($response);
+
+    // Parse xml into PHP array
+    $numOfItems = $parsed_xml->Items->TotalResults;
+    $item_results_array = array(
+        'Id' => 0,
+        'Actor' => "",
+        'Artist' => "",
+        'Author' => "",
+        'CorrectedQuery' => "",
+        'Director' => "",
+        'Keywords' => "",
+        'Manufacturer' => "",
+        'ProductGroup' => "",
+        'Title' => "",
+        'LastUpdated' => "");
+
+    // print_r($results_array);
+
+    if($numOfItems > 0){
+        foreach($parsed_xml->Items->Item as $current){
+            foreach($item_results_array as $entry=>$value){
+                if (isset($current->ItemAttributes->$entry)) {
+                    // print $current->ItemAttributes->$entry . "<br>";
+                    // print "Entry = " . $entry . "<br>";
+                    $item_results_array[$entry] = (string)$current->ItemAttributes->$entry;   
+                }            
+            }
+            
+            if (isset($current->ASIN)) {
+                $item_results_array['ASIN'] = (string)$current->ASIN;
+            }
+
+            if (isset($current->DetailPageURL)) {
+                $item_results_array['DetailPageURL'] = (string)$current->DetailPageURL;
+            }
+
+            $item_results_array['LastUpdated'] = date("Y-m-d H:i:s");
+            
+            // Insert results into MySQL table
+            $sql = sprintf(
+                'INSERT INTO dad_results_table (%s) VALUES ("%s")',
+                implode(',',array_keys($item_results_array)),
+                implode('","',array_values($item_results_array))
+            );
+
+            if (!$con_search->query($sql)) {
+                printf("Errormessage: %s\n", $con_search->error);
+            }
+        }    
+    }
+}
 
 ?>
